3a40b50c284e3d9896b35c38d564ce4db50b270b
YEAHx4 <developerstarlight@gmail.com>
2023-02-03 15:03:38 +0900

    1312

--- src/main.rs
+++ src/main.rs
@@ -21,27 +21,8 @@ fn read_nums() -> Vec<i32> {
 }
 
 fn main() {
-    let mut str = String::new();
-    io::stdin().read_line(&mut str).unwrap();
-    str = String::from(str.trim());
+    let nums = read_nums();
+    let digit = nums[0] as i64 * 10i64.pow(nums[2] as u32) / nums[1] as i64 % 10;
 
-    let mut results = vec![];
-
-    for i in 1..str.len() - 1 {
-        for j in (i + 1)..str.len() {
-            let a = &str[..i];
-            let b = &str[i..j];
-            let c = &str[j..];
-
-            let mut result = String::from("");
-            result.push_str(&a.chars().rev().collect::<String>());
-            result.push_str(&b.chars().rev().collect::<String>());
-            result.push_str(&c.chars().rev().collect::<String>());
-
-            results.push(result);
-        }
-    }
-
-    results.sort();
-    println!("{}", results.first().unwrap());
+    println!("{}", digit);
 }
