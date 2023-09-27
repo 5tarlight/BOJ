23c7a3690c4cc9da08610b7fc31f8a5ed0343c35
YEAHx4 <developerstarlight@gmail.com>
2023-09-27 17:45:26 +0900

    1167

--- boj-py/ps.py
+++ boj-py/ps.py
@@ -1,4 +1,22 @@
-t = int(input())
+import sys
 
-for _ in range(t):
-    m, n, x, y = map(int, input().split())
+v = int(sys.stdin.readline().rstrip())
+
+graph = [[]] * (v + 1)
+
+for _ in range(v):
+    nums = list(map(int, sys.stdin.readline().rstrip().split()))
+
+    r = nums[0]
+    for i in range(len(nums)):
+        if i == 0:
+            continue
+        if nums[i] == -1:
+            break
+
+        if i % 2 == 1:
+            graph[r].append((nums[i], nums[i + 1]))
+        else:
+            continue
+
+print(graph[2])
