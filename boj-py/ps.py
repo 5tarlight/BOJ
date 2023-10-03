a9b8ef6bf28d83871c8eda38e872336a9e8febd1
YEAHx4 <developerstarlight@gmail.com>
2023-10-03 19:34:07 +0900

    1918

--- boj-py/ps.py
+++ boj-py/ps.py
@@ -1,22 +1,11 @@
-import sys
+class Op:
+    def __init__(self, a, b, op):
+        self.a = a
+        self.b = b
+        self.op = op
 
-v = int(sys.stdin.readline().rstrip())
+    def toString(self):
+        return self.a + self.b + self.op
 
-graph = [[]] * (v + 1)
 
-for _ in range(v):
-    nums = list(map(int, sys.stdin.readline().rstrip().split()))
-
-    r = nums[0]
-    for i in range(len(nums)):
-        if i == 0:
-            continue
-        if nums[i] == -1:
-            break
-
-        if i % 2 == 1:
-            graph[r].append((nums[i], nums[i + 1]))
-        else:
-            continue
-
-print(graph[2])
+s = input()
