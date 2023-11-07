85a75d9847c4c4e876f079f414e1e83fa6aa9893
YEAHx4 <developerstarlight@gmail.com>
2023-11-07 13:49:13 +0900

    10926

--- boj-cpp/main.cpp
+++ boj-cpp/main.cpp
@@ -5,34 +5,20 @@
 
 using namespace std;
 
-void init(vector<long long> &a, vector<long long> &tree, int node, int start, int end) {
+typedef long long el;
+typedef vector<el> seg;
+
+void init(seg &a, seg &tree, int node, int start, int end) {
     if (start == end) {
         tree[node] = a[start];
     } else {
         init(a, tree, node * 2, start, (start + end) / 2);
         init(a, tree, node * 2 + 1, (start + end) / 2 + 1, end);
-        tree[node] = min(tree[node * 2], tree[node * 2 + 1]);
+        tree[node] = tree[node * 2] + tree[node * 2 + 1];
     }
 }
 
-long long query(vector<long long> &tree, int node, int start, int end, int left, int right) {
-    if (left > end || right < start)
-        return -1;
-    if (left <= start && end <= right)
-        return tree[node];
-
-    long long lmin = query(tree, node * 2, start, (start + end) / 2, left, right);
-    long long rmin = query(tree, node * 2 + 1, (start + end) / 2 + 1, end, left, right);
-
-    if (lmin == -1)
-        return rmin;
-    else if (rmin == -1)
-        return lmin;
-    else
-        return min(rmin, lmin);
-}
-
-void update(vector<long long> &a, vector<long long> &tree, int node, int start, int end, int index, long long val) {
+void update(seg &a, seg &tree, int node, int start, int end, int index, el val) {
     if (index < start || index > end)
         return;
     if (start == end) {
@@ -42,8 +28,51 @@ void update(vector<long long> &a, vector<long long> &tree, int node, int start, 
     }
     update(a, tree,node * 2, start, (start + end) / 2, index, val);
     update(a, tree,node * 2 + 1, (start + end) / 2 + 1, end, index, val);
-    tree[node] = min(tree[node * 2], tree[node * 2 + 1]);
+    tree[node] = tree[node * 2] + tree[node * 2 + 1];
 }
+
+void updateLazy(seg &tree, seg &lazy, int node, int start, int end) {
+    if (lazy[node] != 0) {
+        tree[node] += (end - start + 1) * lazy[node];
+        if (start != end) {
+            lazy[node * 2] += lazy[node];
+            lazy[node * 2 + 1] += lazy[node];
+        }
+        lazy[node] = 0;
+    }
+}
+
+void updateRange(seg &tree, seg &lazy, int node, int start, int end, int left, int right, el diff) {
+    updateLazy(tree, lazy, node, start, end);
+    if (left > end || right < start)
+        return;
+    if (left <= start && end <= right) {
+        tree[node] += (end - start + 1) * diff;
+        if (start != end) {
+            lazy[node * 2] += diff;
+            lazy[node * 2 + 1] += diff;
+        }
+        return;
+    }
+
+    updateRange(tree, lazy, node * 2, start, (start + end) / 2, left, right, diff);
+    updateRange(tree, lazy, node * 2 + 1, (start + end) / 2 + 1, end, left, right, diff);
+    tree[node] = tree[node * 2] + tree[node * 2 + 1];
+}
+
+el query(seg &tree, seg &lazy, int node, int start, int end, int left, int right) {
+    updateLazy(tree, lazy, node, start, end);
+    if (left > end || right < start)
+        return 0;
+    if (left <= start && end <= right)
+        return tree[node];
+
+    el lmin = query(tree, lazy, node * 2, start, (start + end) / 2, left, right);
+    el rmin = query(tree, lazy, node * 2 + 1, (start + end) / 2 + 1, end, left, right);
+
+    return rmin + lmin;
+}
+
 
 int main() {
     ios::sync_with_stdio(false);
@@ -52,10 +81,12 @@ int main() {
 
     int n;
     cin >> n;
-    vector<long long> a(n);
+    seg a(n);
+
     int h = (int)ceil(log2(n));
     int treeSize = 1 << (h + 1);
-    vector<long long> tree(treeSize);
+    seg tree(treeSize);
+    seg lazy(treeSize);
     for (int i = 0; i < n; i++)
         cin >> a[i];
 
@@ -67,15 +98,14 @@ int main() {
         int op;
         cin >> op;
         if (op == 1) {
-            int i;
-            long long v;
-            cin >> i >> v;
-
-            update(a, tree, 1, 0, n - 1, i - 1, v);
-        } else {
             int left, right;
-            cin >> left >> right;
-            cout << query(tree, 1, 0, n - 1, left - 1, right - 1) << '\n';
+            el diff;
+            cin >> left >> right >> diff;
+
+            updateRange(tree, lazy, 1, 0, n - 1, left - 1, right - 1, diff);
+        } else {
+            int x;
+            cout << query(tree, lazy, 1, 0, n - 1, x - 1, x - 1) << '\n';
         }
     }
 }
