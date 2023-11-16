d9b4a11ec9d066fdfdd1dea6813a4292554d126a
YEAHx4 <developerstarlight@gmail.com>
2023-11-16 23:11:41 +0900

    10807

--- boj-py/ps.py
+++ boj-py/ps.py
@@ -1,11 +1,2 @@
-class Op:
-    def __init__(self, a, b, op):
-        self.a = a
-        self.b = b
-        self.op = op
-
-    def toString(self):
-        return self.a + self.b + self.op
-
-
-s = input()
+input()
+print(map(int, input().split()).count(int(input())))
---
+++ run_py.sh
@@ -0,0 +1,1 @@
+python3 ./boj-py/ps.py
