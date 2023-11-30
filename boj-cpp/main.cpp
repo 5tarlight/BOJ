ec7c16b86ce07bef94439246b1261552ec844f49
YEAHx4 <developerstarlight@gmail.com>
2023-12-01 08:59:46 +0900

    2941

--- boj-cpp/main.cpp
+++ boj-cpp/main.cpp
@@ -1,67 +1,28 @@
 #include <iostream>
-#include <queue>
-#include <cstring>
-
-#define SIZE 100'001
+#include <string>
 
 using namespace std;
-
-int graph[SIZE];
-
-void bfs(int n, int k) {
-    queue<int> q;
-    q.push(n);
-    graph[n] = 1;
-    int minTime = 1e9;
-    int next;
-    int cnt = 0;
-
-    if (n == k) {
-        cout << "0\n1";
-        return;
-    }
-
-    while (!q.empty()) {
-        int now = q.front();
-        q.pop();
-
-        if (minTime < graph[now])
-            break;
-
-        for (int i = 0; i < 3; i++) {
-            if (i == 0)
-                next = now - 1;
-            else if (i == 1)
-                next = now + 1;
-            else
-                next = now * 2;
-
-            if (next < 0 || next > SIZE - 1)
-                continue;
-
-            if (next == k) {
-                minTime = graph[now];
-                cnt++;
-            }
-
-            if (graph[next] == 0 || graph[next] == graph[now] + 1) {
-                q.push(next);
-                graph[next] = graph[now] + 1;
-            }
-        }
-    }
-
-    cout << graph[k] - 1 << '\n';
-    cout << cnt;
-}
 
 int main() {
     ios::sync_with_stdio(false);
     cout.tie(nullptr);
     cin.tie(nullptr);
 
-    int n, k;
-    cin >> n >> k;
+    string s;
+    cin >> s;
+    int l = 0;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == 'c' || s[i] == 'l' || s[i] == 'n' || s[i] == 's' || s[i] == 'z') {
+            l++;
+            i++;
+        } else if (s[i + 1] == '-') {
+            l++;
+            i++;
+        } else {
+            l++;
+            i += 2;
+        }
+    }
 
-    bfs(n, k);
+    cout << l;
 }
