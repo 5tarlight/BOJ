6629f26fac5b5ba1254a069e08cc25cb3e061ab0
YEAHx4 <developerstarlight@gmail.com>
2024-01-25 11:28:23 +0900

    1965

---
+++ boj/silver/1965.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    vector<int> lis;
+    lis.push_back(a[0]);
+
+    for (int i = 1; i < n; i++) {
+        if (lis.back() < a[i])
+            lis.push_back(a[i]);
+        else {
+            int idx = lower_bound(lis.begin(), lis.end(), a[i]) - lis.begin();
+            lis[idx] = a[i];
+        }
+    }
+
+    cout << lis.size();
+}
