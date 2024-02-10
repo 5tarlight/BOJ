54b477e1f8ce7c98e4a90e688344eade920c1991
YEAHx4 <developerstarlight@gmail.com>
2024-02-10 17:26:02 +0900

    2580

--- boj-py/ps.py
+++ boj-py/ps.py
@@ -1,25 +1,13 @@
-n, q = map(int, input().split())
-l = []
+n = int(input())
+k = int(input())
+a = []
+m = {}
 
-for i in range(1, n + 1):
-    l.append((i, 0))
-    
-for _ in range(q):
-    op, v = input().split()
-    if op == '1':
-        x = l[0][0]
-        y = l[0][1]
-        if v == 'U':
-            y += 1
-        elif v == 'R':
-            x += 1
-        elif v == 'D':
-            y -= 1
-        else:
-            x -= 1
-        
-        l.insert(0, (x, y))
-        l.pop()
-    else:
-        v = int(v) - 1
-        print(l[v][0], l[v][1])
+for _ in range(n):
+  a.append(int(input()))
+
+cnt = 0
+def backtrack():
+  global cnt
+  pass
+
