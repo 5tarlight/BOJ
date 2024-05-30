9bb54ca32d5f69c9b573fc974cde108486118948
규산김 <developerstarlight@gmail.com>
2024-05-31 01:38:28 +0900

    1145

--- boj-cpp/main.cpp
+++ boj-cpp/main.cpp
@@ -1,35 +1,10 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
 int main() {
-    int n;
-    string str;
-    cin >> n >> str;
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ios_base::sync_with_stdio(false);
 
-    int ans = 0;
-    int l = 0;
-    int s = 0;
-
-    for (int i = 0; i < n; i++) {
-        if (str[i] == 'S')
-            s++;
-        else if (str[i] == 'L')
-            l++;
-        else if (str[i] == 'R') {
-            if (l == 0)
-                break;
-            ans++;
-            l--;
-        } else if (str[i] == 'K') {
-            if (s == 0)
-                break;
-            ans++;
-            s--;
-        } else
-            ans++;
-    }
-
-    cout << ans;
 }
