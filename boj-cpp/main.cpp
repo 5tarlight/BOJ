3888b0d9335cef78d75660d54dd1305783e3387d
규산김 <developerstarlight@gmail.com>
2024-05-31 01:55:00 +0900

    1672

--- boj-cpp/main.cpp
+++ boj-cpp/main.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
+#include <map>
+#include <string>
 
 using namespace std;
 
 int main() {
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    ios_base::sync_with_stdio(false);
+    map<string, char> d = {
+        {"AA", 'A'},
+        {"AG", 'G'},
+        {"AC", 'A'},
+        {"AT", 'G'},
+        {"GA", 'C'},
+        {"GG", 'G'},
+        {"GC", 'T'},
+        {"GT", 'A'},
+        {"CA", 'A'},
+        {"CG", 'T'},
+        {"CC", 'C'},
+        {"CT", 'G'},
+        {"TA", 'G'},
+        {"TG", 'A'},
+        {"TC", 'G'},
+        {"TT", 'T'}
+    };
 
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    while (s.length() > 1) {
+        s = s.substr(0, s.length() - 2) + d[s.substr(s.length() - 2, 2)];
+    }
+
+    cout << s << endl;
+
+    return 0;
 }
