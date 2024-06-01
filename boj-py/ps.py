ebbd0f4b75192b9122287bd440f6f3ae9ab0002e
규산김 <developerstarlight@gmail.com>
2024-06-01 17:33:10 +0900

    SCSC

--- boj-py/ps.py
+++ boj-py/ps.py
@@ -1,18 +1,9 @@
-import sys
+n = int(input()) - 1
+ans = 8
+d = 1
+while n > 0:
+    n -= d
+    d *= 2
+    ans += 1
 
-n = int(sys.stdin.readline().strip())
-a = list(map(int, sys.stdin.readline().strip().split()))
-
-l = r = 0
-
-ans = 0
-for r in range(n):
-    if r < n - 1 and a[r] > a[r + 1]:
-        cnt = r - l + 1
-        ans += cnt * (cnt + 1) // 2
-        l = r + 1
-
-cnt = r - l + 1
-ans += cnt * (cnt + 1) // 2
-
-print(ans)
+print(ans + 2)
