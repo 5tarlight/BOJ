50cb33dc08bd82e293f96a4cbe3c29e4730ed2bf
규산김 <developerstarlight@gmail.com>
2024-06-10 20:14:04 +0900

    9440

--- boj-py/ps.py
+++ boj-py/ps.py
@@ -1,5 +1,17 @@
-n, s = map(int, input().split())
-a = list(map(int, input().split()))
-cmd = input()
+import sys
 
-print(cmd[::-1])
+while True:
+    a = sorted(list(map(int, sys.stdin.readline().split())))
+    if len(a) == 1:
+        exit(0)
+
+    n1 = n2 = 0
+    first = True
+    for n in a:
+        if first:
+            n1 = 10 * n1 + n
+        else:
+            n2 = 10 * n2 + n
+        first = not first
+
+    print(n1 + n2)
