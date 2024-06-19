52e13ab0443650258ad27260f61ff251a3b89d6f
규산김 <developerstarlight@gmail.com>
2024-06-19 22:05:54 +0900

    9215

--- boj-cpp/main.cpp
+++ boj-cpp/main.cpp
@@ -6,12 +6,4 @@ int main() {
     cin.tie(nullptr);
     cout.tie(nullptr);
     ios_base::sync_with_stdio(false);
-
-    long long t;
-    cin >> t;
-    while (t--) {
-        long long n;
-        cin >> n;
-        cout << n * n << '\n';
-    }
 }
