2f060cb574d8831914b77e486e129fb1791eb48a
YEAHx4 <developerstarlight@gmail.com>
2024-06-24 20:13:42 +0900

    14674

--- boj-cpp/main.cpp
+++ boj-cpp/main.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int n, l, r;
-int dp[101][101][101];
-const int MOD = 1e9 + 7;
-
-#define loop(i, l, r) for (int i = l; i <= r; i++)
-
 int main() {
-    cin >> n >> l >> r;
-    dp[1][1][1] = 1;
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ios_base::sync_with_stdio(false);
 
-    loop(i, 2, n)
-        loop(j, 1, l)
-            loop(k, 1, r)
-                dp[i][j][k] = (
-                        (long long)dp[i - 1][j][k] * (i - 2)
-                        + dp[i - 1][j][k - 1]
-                        + dp[i - 1][j - 1][k]
-                ) % MOD;
+    int n, k;
+    cin >> n >> k;
+    pair<double, pair<int, int>> a[n];
 
-    cout << dp[n][l][r];
+    for (int i = 0; i < n; i++) {
+        int idx, c, h;
+        cin >> idx >> c >> h;
+
+        a[i] = { -(double)h / c, { c, idx }};
+    }
+
+    sort(a, a + n);
+
+    for (int i = 0; i < k; i++)
+        cout << a[i].second.second << '\n';
 }
