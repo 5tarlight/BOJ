9ccd4a9ea5cd9a704f1cbd16de2cb91004038569
YEAHx4 <developerstarlight@gmail.com>
2024-07-05 02:57:01 +0900

    16956

--- boj-py/ps.py
+++ boj-py/ps.py
@@ -1,31 +1,13 @@
-import sys
-input = sys.stdin.readline
+word = input().lower()
+cnt = []
+abc = 'abcdefghijklmnopqrstuvwxyz'
+for i in abc:
+    cnt.append(word.count(i))
 
-n = int(input().strip())
-queue = [[] for _ in range(n)]
-for i in range(n):
-    s = input().strip()
-    queue[i] = list(s.split())
+Max = max(cnt)
 
-s = input().strip()
-for word in s.split():
-    found = False
-    for i in range(n):
-        if len(queue[i]) > 0 and queue[i][0] == word:
-            queue[i].pop(0)
-            found = True
-            break
-
-    if not found:
-        print("Impossible")
-        sys.exit()
-
-ok = True
-for i in range(n):
-    if len(queue[i]) > 0:
-        ok = False
-
-if ok:
-    print("Possible")
+if cnt.count(Max) >= 2:
+    print('?')
 else:
-    print('Impossible')
+    print(abc[cnt.index(Max)].upper())
+
