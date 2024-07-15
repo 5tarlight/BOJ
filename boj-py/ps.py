05df4f232f12be6aae832dc6451fc6d17274fa18
YEAHx4 <developerstarlight@gmail.com>
2024-07-16 02:00:30 +0900

    1446

--- boj-py/ps.py
+++ boj-py/ps.py
@@ -1,4 +1,15 @@
-t = int(input())
-for _ in range(t):
-    a, b = input().split()
-    print(len(a.replace(b,'a')))
+import sys
+
+n = int(input())
+a = []
+for i in range(n):
+    a.append(list(map(int, input().split())))
+
+for i in range(1, n + 1):
+    d = set()
+    for arr in a:
+        d.add(''.join(map(str, arr[:i])))
+    print(d)
+    if len(d) == n:
+        print(i)
+        sys.exit(0)
