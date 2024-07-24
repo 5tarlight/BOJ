da8cedc13ee63806b73281735119a4cb750d83e6
YEAHx4 <developerstarlight@gmail.com>
2024-07-24 20:55:35 +0900

    18512

--- boj-cpp/main.cpp
+++ boj-cpp/main.cpp
@@ -1,28 +1,24 @@
 #include <iostream>
+#include <numeric>
+#include <algorithm>
 
 using namespace std;
-
-int gcd(int a, int b) {
-    return b ? gcd(b, a % b) : a;
-}
 
 int main() {
     cin.tie(nullptr);
     cout.tie(nullptr);
     ios_base::sync_with_stdio(false);
 
-    int n;
-    cin >> n;
-    int g;
-    cin >> g;
-    n--;
-    while (n--) {
-        int num;
-        cin >> num;
-        g = gcd(g, num);
+    int x, y, p1, p2;
+    cin >> x >> y >> p1 >> p2;
+    int lcm = x * y / gcd(x, y);
+
+    for (int i = 1; max(x, y) * i <= lcm; i++) {
+        if (x * i + p1 == y * i + p2) {
+            cout << x * i + p1;
+            return 0;
+        }
     }
 
-    for (int i = 1; i <= g; i++)
-        if (g % i == 0)
-            cout << i << '\n';
+    cout << -1;
 }
