c0349f55b65b63f212b026cdc15278c9797ce9f9
YEAHx4 <developerstarlight@gmail.com>
2024-07-24 19:46:32 +0900

    5618

--- boj-py/ps.py
+++ boj-py/ps.py
@@ -1,8 +1,4 @@
 n, k = map(int, input().split())
-n -= 1
-k -= 1
-f = [1] * 31
-for i in range(1, n + 1):
-    f[i] = f[i - 1] * i
 
-print(f[n] // f[k] // f[n - k])
+import math
+print(math.comb(n - 1, k - 1))
