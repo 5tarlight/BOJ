3bbc71e14973a056a4c8986cd0b1715e865616ee
YEAHx4 <developerstarlight@gmail.com>
2024-08-06 12:06:54 +0900

    2156

--- boj/silver/2156.cpp
+++ boj/silver/2156.cpp
@@ -14,19 +14,11 @@ int main() {
     for (int i = 0; i < n; i++)
         cin >> a[i];
 
-    if (n == 1) {
-        cout << a[0];
-        return 0;
-    }
-
     int dp[n][3];
     dp[0][0] = dp[0][2] = 0;
     dp[0][1] = a[0];
-    dp[1][0] = a[0];
-    dp[1][1] = a[1];
-    dp[1][2] = a[0] + a[1];
 
-    for (int i = 2; i < n; i++) {
+    for (int i = 1; i < n; i++) {
         dp[i][0] = max({ dp[i - 1][2], dp[i - 1][1], dp[i - 1][0] });
         dp[i][1] = dp[i - 1][0] + a[i];
         dp[i][2] = dp[i - 1][1] + a[i];
