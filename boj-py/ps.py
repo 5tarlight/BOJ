dcf4990b43109277fe1ce9d0ad0fad384f2e2a2e
YEAHx4 <developerstarlight@gmail.com>
2024-08-07 11:21:17 +0900

    1790

--- boj-py/ps.py
+++ boj-py/ps.py
@@ -1,4 +1,18 @@
-t = int(input())
-for _ in range(t):
-    input()
-    print(sum(map(int, input().split())))
+n, k = map(int, input().split())
+
+start = 0
+digit = 1
+end = 9
+
+while k > end * digit:
+    k -= digit * end
+    start += end
+    end *= 10
+    digit += 1
+
+ans = start + 1 + (k - 1) // digit
+
+if ans > n:
+    print(-1)
+else:
+    print(str(ans)[(k - 1) % digit])
