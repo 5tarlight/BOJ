82bd93a3ab96e86bf8e51005ac5010d60b69f26b
YEAHx4 <developerstarlight@gmail.com>
2024-08-22 15:32:22 +0900

    1006

--- boj-py/ps.py
+++ boj-py/ps.py
@@ -1,21 +1,6 @@
-while True:
-    s = input()
-    if s == '*': break
+s = input()
 
-    good = True
-    for d in range(len(s) - 2):
-        v = set()
-        if not good:
-            break
-
-        for i in range(len(s) - d - 1):
-            p = s[i] + s[i + d + 1]
-            if p in v:
-                good = False
-                break
-            v.add(p)
-
-    if good:
-        print(s, 'is surprising.')
-    else:
-        print(s, 'is NOT surprising.')
+def getCase(s, target):
+    ans = []
+    if int(s) == target:
+        ans.append(s)
