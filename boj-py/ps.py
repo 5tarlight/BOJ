1fbbc3acce1eabc8e8a38ac4688e5c938f4483cc
YEAHx4 <developerstarlight@gmail.com>
2024-08-24 18:09:57 +0900

    25585

--- boj-py/ps.py
+++ boj-py/ps.py
@@ -1,6 +1,12 @@
-s = input()
+def can_form_rectangle(a, b, c):
+    if a % 2 != 0:
+        return "No"
+    total_area = a + 2 * b + 4 * c
+    if total_area % 4 != 0:
+        return "No"
+    return "Yes"
 
-def getCase(s, target):
-    ans = []
-    if int(s) == target:
-        ans.append(s)
+t = int(input())
+for _ in range(t):
+    a, b, c = map(int, input().split())
+    print(can_form_rectangle(a, b, c))
