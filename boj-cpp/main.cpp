ba5941a0ae7c989a7374520ecdb1e819dc9c1e2c
YEAHx4 <developerstarlight@gmail.com>
2024-08-28 19:14:49 +0900

    32196

--- boj-cpp/main.cpp
+++ boj-cpp/main.cpp
@@ -3,35 +3,22 @@
 
 using namespace std;
 
-long long x, y;
-
-struct Cmp {
-    bool operator()(pair<int, int> &a, pair<int, int> &b) {
-        long long aRes = x * a.first - y * a.second;
-        long long bRes = x * b.first - y * b.second;
-
-        return aRes < bRes;
-    }
-};
-
 int main() {
     cin.tie(nullptr);
     cout.tie(nullptr);
     ios_base::sync_with_stdio(false);
 
-    int n, m, k;
+    long long n, m, k, x, y;
     cin >> n >> m >> k >> x >> y;
-    pair<int, int> a[n];
-    for (int i = 0; i < n; i++)
-        cin >> a[i].first >> a[i].second;
-    sort(a, a + n, Cmp());
-
-    long long normal = k;
-    long long fast = k;
-    for (int i = 0; i < m; i++) {
-        normal += a[i].first;
-        fast -= a[i].second;
+    auto ans = k * (x + y);
+    long long a[n];
+    for (int i = 0; i < n; i++) {
+        int n1, n2;
+        cin >> n1 >> n2;
+        a[i] = x * n1 - y * n2;
     }
-
-    cout << normal * x + fast * y;
+    sort(a, a + n);
+    for (int i = 0; i < m; i++)
+        ans += a[i];
+    cout << ans;
 }
