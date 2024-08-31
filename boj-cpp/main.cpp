fed6936d2bcd97fa8a5327e6094d57e5a2dd50ae
YEAHx4 <developerstarlight@gmail.com>
2024-08-31 23:34:07 +0900

    13118

--- boj-cpp/main.cpp
+++ boj-cpp/main.cpp
@@ -7,47 +7,4 @@ int main() {
     cout.tie(nullptr);
     ios_base::sync_with_stdio(false);
 
-    int l, t, n;
-    cin >> l >> t >> n;
-    vector<int> loc;
-
-    while (n--) {
-        int x;
-        char d;
-        cin >> x >> d;
-
-        if (d == 'L') {
-            int tmp = t - x;
-            if (tmp < 0)
-                x -= t;
-            else {
-                int a = tmp / l;
-                int b = tmp % l;
-
-                if (a % 2 == 1)
-                    x = l - b;
-                else
-                    x = b;
-            }
-        } else {
-            int tmp = t - (l - x);
-            if (tmp < 0)
-                x += t;
-            else {
-                int a = tmp / l;
-                int b = tmp % l;
-                if (a % 2 == 1)
-                    x = b;
-                else
-                    x = l - b;
-            }
-        }
-
-        loc.push_back(x);
-    }
-
-    sort(loc.begin(), loc.end());
-
-    for (int v : loc)
-        cout << v <<  ' ';
 }
