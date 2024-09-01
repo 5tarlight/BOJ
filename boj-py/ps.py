f184812c09fd84426574fbab170b3ab7dbf22553
YEAHx4 <developerstarlight@gmail.com>
2024-09-02 01:39:26 +0900

    30022

--- boj-py/ps.py
+++ boj-py/ps.py
@@ -1,10 +1,23 @@
-st = set()
-n = int(input())
-s = input()
+t = int(input())
+for _ in range(t):
+    cnt = [0] * 10
+    all = 0
+    for i in range(1, 10):
+        cnt[i] = int(input())
+        all += cnt[i]
 
-for i in range(n + 1):
-    for ch in 'abcdefghijklmnopqrstuvwxyz':
-        target = s[:i] + ch + s[i:]
-        st.add(target)
+    cnt[9] += cnt[6]
+    cnt[6] = 0
 
-print(len(st))
+    idx = 9
+    ans = [0] * all
+    for i in range(all):
+        while cnt[idx] == 0:
+            idx -= 1
+        if i % 2 == 0:
+            ans[i // 2] = idx
+        else:
+            ans[all - i // 2 - 1] = idx
+        cnt[idx] -= 1
+
+    print(' '.join(map(str, ans)))
