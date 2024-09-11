f9c7b3e1eaef5bebf482ea68fe7aa32523ffb6c7
YEAHx4 <developerstarlight@gmail.com>
2024-09-11 09:19:25 +0900

    10101

--- boj/bronze/10101.cpp
+++ boj/bronze/10101.cpp
@@ -1,23 +1,13 @@
-#include <iostream>
+#include "bits/stdc++.h"
 
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cout.tie(nullptr);
-    cin.tie(nullptr);
-
     int a, b, c;
     cin >> a >> b >> c;
 
-    if (a == b & b == c && c == a)
-        cout << "Equilateral";
-    else if (a + b + c == 180) {
-        if (a == b || b == c || c == a)
-            cout << "Isosceles";
-        else
-            cout << "Scalene";
-    } else {
-        cout << "Error";
-    }
+    if (a + b + c != 180) cout << "Error";
+    else if (a == 60 && b == 60 && c == 60) cout << "Equilateral";
+    else if (a == b || b == c || c == a) cout << "Isosceles";
+    else cout << "Scalene";
 }
