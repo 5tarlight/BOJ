981383506526f0cd69a4609327197b778c7c221d
YEAHx4 <developerstarlight@gmail.com>
2024-09-26 11:52:42 +0900

    15480

--- boj-cpp/main.cpp
+++ boj-cpp/main.cpp
@@ -2,25 +2,44 @@
 
 using namespace std;
 
-constexpr int MAX = 100000;
+int n, m;
+vector<int> graph[100001];
+vector<int> tree[100001];
+int depth[100001];
+int p[100001][18];
 
-struct Line {
-    long long a, b;
-    double s;
-
-    Line(long long _a, long long _b, double _s = 0)
-        : a(_a), b(_b), s(_s) {}
-};
-
-double cross(const Line &a, const Line &b) {
-    return (double)(a.b - b.b) / (a.a - b.a);
+void init(int cur) {
+    for (int nxt : graph[cur]) {
+        if (depth[cur] == -1) {
+            depth[nxt] = depth[cur] + 1;
+            p[nxt][0] = cur;
+            init(nxt);
+        }
+    }
 }
 
-int a[MAX];
-int b[MAX];
-int n;
-vector<Line> s;
-long long dp[MAX];
+int lca(int u, int v) {
+    if (depth[u] < depth[v]) swap(u, v);
+
+    int diff = depth[u] - depth[v];
+    for (int j = 0; diff; j++) {
+        if (diff % 2) u = p[u][j];
+        diff /= 2;
+    }
+
+    if (u != v) {
+        for (int j = 17; j >= 0; j--) {
+            if (p[u][j] != -1 && p[u][j] != p[v][j]) {
+                u = p[u][j];
+                v = p[v][j];
+            }
+        }
+
+        u = p[u][0];
+    }
+
+    return u;
+}
 
 int main() {
     cin.tie(nullptr);
@@ -28,43 +47,43 @@ int main() {
     ios_base::sync_with_stdio(false);
 
     cin >> n;
-
-    for (int i = 0; i < n; i++)
-        cin >> a[i];
-    for (int i = 0; i < n; i++)
-        cin >> b[i];
-
-    for (int i = 1; i < n; i++) {
-        Line g(b[i - 1], dp[i - 1]);
-
-        while (s.size() >= 1) {
-            g.s = cross(g, s.back());
-
-            if (g.s < s.back().s)
-                s.pop_back();
-            else
-                break;
-        }
-
-        s.push_back(g);
-
-        long long x = a[i];
-        int pos = 0;
-
-        int left = 0;
-        int right = s.size() - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-
-            if (s[mid].s < x) {
-                pos = mid;
-                left = mid + 1;
-            } else
-                right = mid - 1;
-        }
-
-        dp[i] = s[pos].a * x + s[pos].b;
+    for (int i = 0; i < n - 1; i++) {
+        int u, v;
+        cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
     }
 
-    cout << dp[n - 1];
+    memset(depth, -1, sizeof(depth));
+    memset(p, -1, sizeof(p));
+    depth[1] = 0;
+    init(1);
+
+    for (int j = 0; j < 18; j++)
+        for (int i = 1; i <= n; i++)
+            if (p[i][j] != -1)
+                p[i][j + 1] = p[p[i][j]][j];
+
+    cin >> m;
+    while (m--) {
+        int r, u, v;
+        cin >> r >> u >> v;
+
+        int rr = r;
+        int uu = u;
+        int vv = v;
+
+        int uv = lca(u, v);
+        u = uu; v = vv;
+        int ur = lca(u, r);
+        u = uu; r = rr;
+        int vr = lca(v, r);
+        v = vv; r = rr;
+
+        int ans = uv;
+        ans = depth[ans] > depth[ur] ? ans : ur;
+        ans = depth[ans] > depth[vr] ? ans : vr;
+
+        cout << ans << '\n';
+    }
 }
