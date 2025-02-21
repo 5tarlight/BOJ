2018c3f684421d9fd71ff4e3662fc790660da811
YEAHx4 <developerstarlight@gmail.com>
2025-02-21 16:48:20 +0900

    Cpp git util: validation

--- cpp.sh
+++ cpp.sh
@@ -1,4 +1,17 @@
-mkdir ./boj/$1
+levels=("except" "bronze" "silver" "gold" "platinum" "diamond" "ruby")
+
+# Check level input correctly
+if [[ ! " ${levels[@]} " =~ " $1 " ]]; then
+    echo "Error: Invalid difficulty level. Choose from: ${levels[*]}"
+    exit 1
+fi
+
+# Create folder if not exists
+if [[ ! -d "./boj/$1" ]]; then
+    mkdir ./boj/$1
+fi
+
 cp ./boj-cpp/main.cpp ./boj/$1/$2.cpp
 git add .
 git commit -m "$2"
+j
