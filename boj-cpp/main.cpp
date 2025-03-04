ac05748be84d88a08d9aceb411e0b73f2260bc8f
YEAHx4 <developerstarlight@gmail.com>
2025-03-04 11:14:46 +0900

    19946

--- boj-cpp/main.cpp
+++ boj-cpp/main.cpp
@@ -7,17 +7,5 @@ int main() {
     cout.tie(nullptr);
     ios_base::sync_with_stdio(false);
 
-    map<string, int> cnt;
-    string s;
-    int total = 0;
-    while (getline(cin, s)) {
-        cnt[s]++;
-        total++;
-    }
-
-    cout.precision(4);
-    cout << fixed;
-
-    for (const auto& [s, c] : cnt)
-        cout << s << " " << 100.0 * c / (double)total << '\n';
+    unsigned long long n = 18'446'744'073'709'551'616ULL;
 }
