ca78ea69534415b540a67973c83918271c33a32a
YEAHx4 <developerstarlight@gmail.com>
2025-03-17 17:01:27 +0900

    C++ template

--- boj-cpp/main.cpp
+++ boj-cpp/main.cpp
@@ -2,55 +2,23 @@
 
 using namespace std;
 
+struct fast_ios { fast_ios(){ cin.tie(nullptr), ios::sync_with_stdio(false), cout << fixed << setprecision(20); }; } fast_ios_;
+
+const std::vector<std::pair<int, int>> grid_dxs{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+template <class OStream, class T> OStream &operator<<(OStream &os, const std::vector<T> &vec) { os << '['; for (auto v : vec) os << v << ','; os << ']'; return os; }
+template <class OStream, class T, class U> OStream &operator<<(OStream &os, const std::pair<T, U> &pa) { return os << '(' << pa.first << ',' << pa.second << ')'; }
+template <class T1, class T2> std::pair<T1, T2> operator+(const std::pair<T1, T2> &l, const std::pair<T1, T2> &r) { return std::make_pair(l.first + r.first, l.second + r.second); }
+template <class T1, class T2> std::pair<T1, T2> operator-(const std::pair<T1, T2> &l, const std::pair<T1, T2> &r) { return std::make_pair(l.first - r.first, l.second - r.second); }
+
+#ifdef YEAHx4
+const string COLOR_RESET = "\033[0m", BRIGHT_GREEN = "\033[1;32m", BRIGHT_RED = "\033[1;31m", BRIGHT_CYAN = "\033[1;36m", NORMAL_CROSSED = "\033[0;9;37m", RED_BACKGROUND = "\033[1;41m", NORMAL_FAINT = "\033[0;2m";
+#define dbg(x) std::cerr << BRIGHT_CYAN << #x << COLOR_RESET << " = " << (x) << NORMAL_FAINT << " (L" << __LINE__ << ") " << __FILE__ << COLOR_RESET << std::endl
+#define dbgif(cond, x) ((cond) ? std::cerr << BRIGHT_CYAN << #x << COLOR_RESET << " = " << (x) << NORMAL_FAINT << " (L" << __LINE__ << ") " << __FILE__ << COLOR_RESET << std::endl : std::cerr)
+#else
+#define dbg(x) ((void)0)
+#define dbgif(cond, x) ((void)0)
+#endif
+
 int main() {
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    ios_base::sync_with_stdio(false);
-
-    int n;
-    cin >> n;
-    int dp[n][n][3];
-    memset(dp, 0, sizeof(dp));
-    bool ok[n][n];
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cin >> ok[i][j];
-            ok[i][j] = !ok[i][j];
-        }
-    }
-
-    // 0 : -, 1 : \, 2 : |
-    dp[0][1][0] = 1;
-
-    for (int j = 0; j < n; j++) {
-        for (int i = 0; i < n; i++) {
-            if (!ok[i][j]) continue;
-            for (int k = 0; k < 3; k++) {
-                if (dp[i][j][k]) continue;
-
-                if (k == 0) {
-                    if (j - 1 >= 0 && ok[i][j - 1]) {
-                        dp[i][j][k] += dp[i][j - 1][0];
-                        dp[i][j][k] += dp[i][j - 1][1];
-                    }
-                } else if (k == 1) {
-                    if (j - 1 >= 0 && i - 1 >= 0 && ok[i][j - 1] && ok[i - 1][j]) {
-                        if (ok[i][j - 1] && ok[i - 1][j]) {
-                            dp[i][j][k] += dp[i - 1][j - 1][0];
-                            dp[i][j][k] += dp[i - 1][j - 1][1];
-                            dp[i][j][k] += dp[i - 1][j - 1][2];
-                        }
-                    }
-                } else {
-                    if (i - 1 >= 0 && ok[i - 1][j]) {
-                        dp[i][j][k] += dp[i - 1][j][1];
-                        dp[i][j][k] += dp[i - 1][j][2];
-                    }
-                }
-            }
-        }
-    }
-
-    cout << dp[n - 1][n - 1][0] + dp[n - 1][n - 1][1] + dp[n - 1][n - 1][2];
 }
--- boj-py/ps.py
+++ boj-py/ps.py
@@ -1,5 +1,25 @@
-from math import lcm
+def isPrime(n):
+    if n % 3 == 0: return 3
 
-n = int(input())
-a = map(int, [input() for _ in range(n)])
-print(lcm(*a))
+    i = 5
+    while i * i <= n:
+        if n % i == 0:
+            return i
+        i += 2
+
+    return -1
+
+for _ in range(int(input())):
+    n = input()
+
+    tmp = isPrime(int(n))
+
+    if tmp != -1:
+        print(0, tmp)
+    else:
+        for i in range(len(n)):
+            tmp = isPrime(int(n[:i] + n[i+1:]))
+
+            if tmp != -1:
+                print(i, tmp)
+                break
