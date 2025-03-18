89644345d7b293100893e971da6c66388efbb23c
YEAHx4 <developerstarlight@gmail.com>
2025-03-18 13:06:27 +0900

    29724

--- boj-py/ps.py
+++ boj-py/ps.py
@@ -1,25 +1,2 @@
-def isPrime(n):
-    if n % 3 == 0: return 3
-
-    i = 5
-    while i * i <= n:
-        if n % i == 0:
-            return i
-        i += 2
-
-    return -1
-
-for _ in range(int(input())):
-    n = input()
-
-    tmp = isPrime(int(n))
-
-    if tmp != -1:
-        print(0, tmp)
-    else:
-        for i in range(len(n)):
-            tmp = isPrime(int(n[:i] + n[i+1:]))
-
-            if tmp != -1:
-                print(i, tmp)
-                break
+s1 = input()
+s2 = input()
