8924f9a3caeaffc0ebb029ba8e39345a612b8854
YEAHx4 <developerstarlight@gmail.com>
2025-03-20 19:06:23 +0900

    Update trie

--- codebook/string/trie.cpp
+++ codebook/string/trie.cpp
@@ -1,31 +1,44 @@
 struct Trie {
-    bool end;
-    Trie *nxt[26];
+  Trie *ch[26];
+  bool end;
 
-    Trie() : end(false) { memset(nxt, 0, sizeof(nxt)); }
+  Trie() {
+    end = false;
+    for (int i = 0; i < 26; i++)
+      ch[i] = nullptr;
+  }
 
-    ~Trie() {
-        for (int i = 0; i < 26; i++)
-            delete nxt[i];
+  ~Trie() {
+    for (int i = 0; i < 26; i++)
+      delete ch[i];
+  }
+
+  void insert(const char *s) {
+    if (!*s) {
+      this->end = true;
+      return;
     }
+    int cur = *s - 'A';
+    if (!ch[cur])
+      ch[cur] = new Trie;
+    ch[cur]->insert(s + 1);
+  }
 
-    void insert(const char *key) {
-        if (*key == '\0')
-            end = true;
-        else {
-            int cur = *key - 'A';
-            if (nxt[cur] == NULL)
-                nxt[cur] = new Trie();
-            nxt[cur]->insert(key + 1);
-        }
-    }
+  bool find(const char *s) {
+    if (!*s)
+      return this->end;
+    int cur = *s - 'A';
+    if (!ch[cur])
+      return false;
+    return ch[cur]->find(s + 1);
+  }
 
-    Trie *find(const char *key) {
-        if (*key == '\0')
-            return this;
-        int cur = *key - 'A';
-        if (nxt[cur] == NULL)
-            return NULL;
-        return nxt[cur]->find(key + 1);
-    }
-};
+  bool findPrefix(const char *s) {
+    if (!*s)
+      return true;
+    int cur = *s - 'A';
+    if (!ch[cur])
+      return false;
+    return ch[cur]->findPrefix(s + 1);
+  }
+}
