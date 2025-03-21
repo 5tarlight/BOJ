8e70d2a3ade66b14f480511f8346f54a1c11a849
YEAHx4 <developerstarlight@gmail.com>
2025-03-21 11:52:50 +0900

    2343

--- boj-py/ps.py
+++ boj-py/ps.py
@@ -1,2 +1,16 @@
-t, tz = input().split()
-h, m = map(int, t.split(':'))
+n = int(input())
+
+dp = [-1, -1, -1, 1, -1, 1]
+
+for i in range(6, n + 1):
+    tmp = 10 ** 9
+    if dp[i - 3] != -1:
+        tmp = min(tmp, dp[i - 3] + 1)
+    if dp[i - 5] != -1:
+        tmp = min(tmp, dp[i - 5] + 1)
+    if tmp == 10 ** 9:
+        dp.append(-1)
+    else:
+        dp.append(tmp)
+
+print(dp[n])
