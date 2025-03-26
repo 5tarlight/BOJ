a96661e3b5f5c9f123578d244173aab5597e7f55
YEAHx4 <developerstarlight@gmail.com>
2025-03-26 12:06:59 +0900

    7774

--- template.cpp
+++ template.cpp
@@ -5,7 +5,7 @@ void fio() { cin.tie(nullptr); ios::sync_with_stdio(false); }
 template <class OStream, class T> OStream &operator<<(OStream &os, const std::vector<T> &vec) { os << '['; for (auto v : vec) os << v << ','; os << ']'; return os; }
 template <class OStream, class T, class U> OStream &operator<<(OStream &os, const std::pair<T, U> &pa) { return os << '(' << pa.first << ',' << pa.second << ')'; }
 #ifdef YEAHx4
-#define dbg(x) std::cout << #x << " = " << (x) << " (L" << __LINE__ << ") " << __FILE__ << endl
+#define dbg(x) std::cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
 #else
 #define dbg(x) ((void)0)
 #endif
