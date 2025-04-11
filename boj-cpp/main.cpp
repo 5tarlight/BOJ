e77c37fe7d497aac14f9f6d7c00db82e3879c7b0
YEAHx4 <developerstarlight@gmail.com>
2025-04-11 13:10:51 +0900

    cout tie

--- boj-cpp/main.cpp
+++ boj-cpp/main.cpp
@@ -1,29 +1,15 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-void fio() { cin.tie(nullptr); ios::sync_with_stdio(false); }
-
-using ll = long long;
-
-constexpr ll MOD = 1e9 + 7;
-
-ll modpow(ll a, ll b) {
-    ll ans = 1;
-
-    while (b) {
-        if (b & 1)
-            ans = (ans * a) % MOD;
-        a = (a * a) % MOD;
-        b >>= 1;
-    }
-
-    return ans;
-}
+void fio() { cin.tie(nullptr); cout.tie(nullptr); ios::sync_with_stdio(false); }
+template <class OStream, class T> OStream &operator<<(OStream &os, const std::vector<T> &vec) { os << '['; for (auto v : vec) os << v << ','; os << ']'; return os; }
+template <class OStream, class T, class U> OStream &operator<<(OStream &os, const std::pair<T, U> &pa) { return os << '(' << pa.first << ',' << pa.second << ')'; }
+#ifdef YEAHx4
+#define dbg(x) std::cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+#else
+#define dbg(x) ((void)0)
+#endif
 
 int main() {
     fio();
-
-    int a, b, n;
-    cin >> a >> b >> n;
-    cout << modpow(modpow(2, 31), n - 1);
 }
--- template.cpp
+++ template.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-void fio() { cin.tie(nullptr); ios::sync_with_stdio(false); }
+void fio() { cin.tie(nullptr); cout.tie(nullptr); ios::sync_with_stdio(false); }
 template <class OStream, class T> OStream &operator<<(OStream &os, const std::vector<T> &vec) { os << '['; for (auto v : vec) os << v << ','; os << ']'; return os; }
 template <class OStream, class T, class U> OStream &operator<<(OStream &os, const std::pair<T, U> &pa) { return os << '(' << pa.first << ',' << pa.second << ')'; }
 #ifdef YEAHx4
