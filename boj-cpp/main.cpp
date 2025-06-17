42e6021f9a1dd7e3693fd7c5725f69aab3e502d4
YEAHx4 <developerstarlight@gmail.com>
2025-06-17 14:16:39 +0900

    1038

--- boj-cpp/main.cpp
+++ boj-cpp/main.cpp
@@ -5,25 +5,4 @@ void fio() { cin.tie(nullptr); cout.tie(nullptr); ios::sync_with_stdio(false); }
 
 int main() {
     fio();
-
-    int n, k;
-    cin >> n >> k;
-    int cnt = __builtin_popcount(n);
-
-    int ans = 0;
-    int tmp = 1;
-    while (cnt > k && tmp <= n) {
-        if (n & tmp) {
-            n += tmp;
-            cnt = __builtin_popcount(n);
-            ans += tmp;
-        }
-        tmp <<= 1;
-    }
-
-    if (cnt <= k) {
-        cout << ans << endl;
-    } else {
-        cout << -1 << endl;
-    }
 }
