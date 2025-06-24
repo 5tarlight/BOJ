1462c2bc6956d375e51c5c2376e1d34b730e4428
YEAHx4 <developerstarlight@gmail.com>
2025-06-24 19:30:29 +0900

    1015

--- boj-py/ps.py
+++ boj-py/ps.py
@@ -1,16 +1,33 @@
-from itertools import combinations
+import sys
+input = sys.stdin.readline
 
-nums = []
+v, e = map(int, input().split())
+k = int(input())
+graph = [[int(1e9)] * (v + 1) for _ in range(v + 1)]
+dist = [int(1e9)] * (v + 1)
+visited = [False] * (v + 1)
 
-for n in range(1, 11):
-    for c in combinations(range(10), n):
-        num = int(''.join(map(str, sorted(c, reverse=True))))
-        nums.append(num)
+for _ in range(e):
+    u, vv, w = map(int, input().split())
+    graph[u][vv] = min(graph[u][v], w)
 
-nums.sort()
+dist[k] = 0
+for _ in range(1, v + 1):
+    minIdx = -1
+    for j in range(1, v + 1):
+        if not visited[j] and (minIdx == -1 or dist[j] < dist[minIdx]):
+            minIdx = j
 
-n = int(input())
-if n >= len(nums):
-    print(-1)
-else:
-    print(nums[n])
+    if minIdx == -1:
+        break
+    visited[minIdx] = True
+
+    for j in range(1, v + 1):
+        if not visited[j] and graph[minIdx][j] != int(1e9):
+            dist[j] = min(dist[j], dist[minIdx] + graph[minIdx][j])
+
+for i in range(1, v + 1):
+    if dist[i] == int(1e9):
+        print("INF")
+    else:
+        print(dist[i])
