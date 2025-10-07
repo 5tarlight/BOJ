509b665e52c56684f534a28004985b20ae5225ec
YEAHx4 <developerstarlight@gmail.com>
2025-10-07 14:22:14 +0900

    14891

--- boj-py/ps.py
+++ boj-py/ps.py
@@ -1,21 +1,8 @@
-import sys
-input = sys.stdin.readline
+a, b = map(int, input().split())
 
-t = int(input())
+gcd = 1
+for i in range(2, min(a, b)):
+    if a % i == 0 and b % i == 0:
+        gcd = i
 
-for _ in range(t):
-    input()
-    r, c = map(int, input().split())
-    s = [input().strip() for _ in range(r)]
-
-    ans = 0
-    for i in range(r):
-        for j in range(c - 2):
-            if s[i][j] == '>' and s[i][j + 1] == 'o' and s[i][j + 2] == '<':
-                ans += 1
-
-    for j in range(c):
-        for i in range(r - 2):
-            if s[i][j] == 'v' and s[i + 1][j] == 'o' and s[i + 2][j] == '^':
-                ans += 1
-    print(ans)
+print(gcd)
