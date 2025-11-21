e02c4407feea8f0cd79701f39c5355cb67653d35
YEAHx4 <developerstarlight@gmail.com>
2025-11-21 14:56:10 +0900

    31851

--- boj-py/ps.py
+++ boj-py/ps.py
@@ -1,28 +1,13 @@
-import sys
+import heapq
 
-input = sys.stdin.readline
-INF = int(1e9)
+pq = []
+heapq.heapify(pq)
 
-n, m = map(int, input().split())
-graph = [[] for _ in range(n + 1)] # {vertex, weight}
+n = int(input())
 
-for _ in range(m):
-    a, b, w = map(int, input().split())
-    graph[a].append((b, w))
+for _ in range(n):
+    a = int(input())
+    heapq.heappush(pq, (abs(a), a))
 
-dist = [INF for _ in range(n + 1)]
-dist[1] = 0
-
-for k in range(1, n + 1):
-    for i in range(1, n + 1):
-        for nxt, cost in graph[i]:
-            if dist[i] != 1e9 and dist[nxt] > dist[i] + cost:
-                dist[nxt] = dist[i] + cost
-
-                if k == n:
-                    print(-1)
-                    sys.exit(0)
-
-for i in range(2, n + 1):
-    if dist[i] == 1e9: print(-1)
-    else: print(dist[i])
+while pq:
+    print(heapq.heappop(pq)[1], end=' ')
